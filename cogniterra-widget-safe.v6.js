412b1ed01c83cec16899e783c1ad8bec192663fa
info-cogniterra <info@cogniterra.cz>
2025-10-16 18:52:06 +0200

    Update cogniterra-widget-safe.v6.js

--- cogniterra-widget-safe.v6.js
+++ cogniterra-widget-safe.v6.js
@@ -223,4 +223,3 @@ function renderLeadBoxPricing(params){
   cgSafeStart();
   send.addEventListener('click',()=>{ const q=ta.value; ta.value=''; ask(q); });
   ta.addEventListener('keydown',(e)=>{ if(e.key==='Enter'&&!e.shiftKey){ e.preventDefault(); const q=ta.value; ta.value=''; ask(q); }});
-})();
